22a70b71e40f81ce4a4d8673cd8b2861e3437091
Bảo Nguyên <baonguyenyam@gmail.com>
2018-06-08 21:28:54 +0700

    add face com

--- assets/scripts.js
+++ assets/scripts.js
@@ -6,6 +6,7 @@ function createNewsSocial1() {
   var newsFullPath = document.URL
   var newsFullPathEncode = encodeURIComponent(document.URL)
   $('.fb-share-button').attr('data-href', newsFullPath)
+  $('.fb-comments').attr('data-href', newsFullPath)
   $('.fb-share-button .fb-xfbml-parse-ignore').attr('href', 'https://www.facebook.com/sharer/sharer.php?u=' + newsFullPathEncode + '&src=sdkpreparse')
   $('.twitter-share-button').attr('data-url', newsFullPath)
 }
